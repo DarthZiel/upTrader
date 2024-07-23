4b7061354c82b08337f91ca6b2f2d6b638777afa
DarthZiel <Studerzhan@gmail.com>
2024-07-23 19:48:59 +0500

    create models

--- TreeMenu/models.py
+++ TreeMenu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class TreeMenu(models.Model):
+    title = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='children',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.title
